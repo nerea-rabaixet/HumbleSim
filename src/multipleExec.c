66296fc933bc7dacdfaec1bf43541dd9e4e0c98e
nerea-rabaixet <72511251+nerea-rabaixet@users.noreply.github.com>
2023-04-13 02:57:29 -0400

    Update multipleExec.c

--- src/multipleExec.c
+++ src/multipleExec.c
@@ -1,36 +1,70 @@
-#include <iostream>
-#include <stdexcept>
 #include <stdio.h>
-#include <string>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
 
-using namespace std;
+int main(void) {
+    int i, fd, lock, size, status;
+    char cmd[50], *result;
+    FILE *fp;
 
-string exec(string command) {
-   char buffer[128];
-   string result = "";
+    // Open CSV file for writing and write header
+    fp = fopen("results_mult.csv", "w");
+    if (fp == NULL) {
+        printf("Error creating file!\n");
+        exit(1);
+    }
+    fprintf(fp, "i,result\n");
 
-   // Open pipe to file
-   FILE* pipe = popen(command.c_str(), "r");
-   if (!pipe) {
-      return "popen failed!";
-   }
+    // Execute shell command for values of i from 1 to 10
+    for (i = 1; i <= 10; i++) {
+        // Acquire lock on CSV file
+        fd = fileno(fp);
+        lock = flock(fd, LOCK_EX);
+        if (lock == -1) {
+            printf("Error acquiring lock!\n");
+            exit(1);
+        }
 
-   // read till end of process:
-   while (!feof(pipe)) {
+        // Execute shell command and read result
+        sprintf(cmd, "./HumbleSim 1000 1 1 10 %d 2 > temp.txt", i);
+        status = system(cmd);
+        if (status != 0) {
+            printf("Error executing command!\n");
+            exit(1);
+        }
 
-      // use buffer to read and add to result
-      if (fgets(buffer, 128, pipe) != NULL)
-         result += buffer;
-   }
+        // Read contents of temporary file into memory
+        FILE *temp = fopen("temp.txt", "r");
+        fseek(temp, 0L, SEEK_END);
+        size = ftell(temp);
+        fseek(temp, 0L, SEEK_SET);
+        result = malloc(size + 1);
+        fread(result, size, 1, temp);
+        result[size] = '\0';
+        fclose(temp);
 
-   pclose(pipe);
-   return result;
-}
+        // Enclose result in double quotes and write to CSV file
+        fprintf(fp, "%d,\"%s\"", i, result);
 
-int main() {
-  for(int i = 10;i<15;i++){
-    string ls = exec("1000", "1", "1", (char *)(&i),"2"");
-  }
-   string ls = exec("ls");
-   cout << ls;
+        // Release lock on CSV file
+        lock = flock(fd, LOCK_UN);
+        if (lock == -1) {
+            printf("Error releasing lock!\n");
+            exit(1);
+        }
+
+        // Print result to console
+        printf("Result for i=%d: %s", i, result);
+
+        // Free result buffer
+        free(result);
+    }
+
+    // Close CSV file
+    fclose(fp);
+
+    return 0;
 }
